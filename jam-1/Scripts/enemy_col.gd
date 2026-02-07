c8ea2d3fca86896a9232108c04298b5755333e90
jfdiazbus <juan-felipe.diaz-bustamante@epitech.eu>
2026-02-07 14:32:58 +0100

    add coin animation player animation && score also levels and think i the bug

--- jam-1/Scripts/enemy_col.gd
+++ jam-1/Scripts/enemy_col.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
 
-const SPEED = 40
+const SPEED = 80
 
 var direction = 1
 
 @onready var ray_cast_right = $ColRight
 @onready var ray_cast_left = $ColLeft
+@onready var an_sprite = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
+		an_sprite.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
+		an_sprite.flip_h = false
 		
 	position.x += direction * SPEED * delta
